204d29e7c0f1e17ee93d2758d03406e5bb86b01b
'yunghyunchoi' <'asl321@naver.com'>
2023-03-07 14:10:56 +0900

    a

---
+++ src/main/java/com/my/spring/web/ch02/ex01/UserController.java
@@ -0,0 +1,28 @@
+package com.my.spring.web.ch02.ex01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	@GetMapping("11")
+	public ModelAndView handler11(ModelAndView mv) {
+		mv.addObject("user", new User("최한석", 11));
+		mv.setViewName("ch02/ex01/user");
+		
+		return mv;
+	}
+	
+	@GetMapping("21")
+	public String handler21(Model model) {
+		model.addAttribute("user", new User("한아름", 21));
+		return "ch02/ex01/user";
+	}
+	
+	@GetMapping("22")
+	public String handler22() {
+		return "ch02/ex01/user";
+	}
+}
